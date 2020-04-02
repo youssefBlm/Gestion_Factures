22f738ae0ee621300baf2684d16baf9e65940c07
youssef <youssefblm@live.fr>
2020-04-02 03:44:44 +0200

    correction sur list

--- views/common/navbar.php
+++ views/common/navbar.php
@@ -66,7 +66,7 @@ if (!isset($_SESSION["Loged"])) {
      <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
        <div class="bg-white py-2 collapse-inner rounded">
          <h6 class="collapse-header">Actions :</h6>
-         <a class="collapse-item" href="index.php?c=employee&m=list">lol4</a>
+         <a class="collapse-item" href="index.php?c=employee&m=liste">lol4</a>
          <a class="collapse-item" href="/AppMvc/tasks/index">lolo</a>
          <a class="collapse-item" href="/AppMvc/tasks/reglageLangue">lklkl</a>
        </div>
